d2ac7f687407be80925bfabc9db263f43a125c54
Sandyp <sandyphendro@gmail.com>
2016-08-21 17:55:00 +0545

    BUG FIXING: Can't have more than one operator

--- js/script.js
+++ js/script.js
@@ -2,47 +2,60 @@
  * Created by saroj on 8/15/16.
  */
 
-"use strict"
+"use strict";
 
 var keys = document.querySelectorAll("#calculator span");
 var operators = ['+', '-', 'x', '÷'];
-var p  = document.querySelector("#result");
-
+var p = document.querySelector("#result");
 
 // Add onclick event to all the keys and perform operations
-for(var i = 0; i < keys.length; i++) {
-    keys[i].onclick = function(e) {
+for (var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function (e) {
         // Get the input and button values
         var input = document.querySelector('p');
         var inputVal = input.innerHTML;
         var btnVal = this.innerHTML;
 
-        if (btnVal == 'C'){
+        if (btnVal == 'C') {
             input.innerHTML = '';
         }
         else if (btnVal == '=') {
             var equation = inputVal;
 
-
-            if (equation)
+            if (equation) {
                 input.innerHTML = eval(equation);
+            }
         }
-        else  {
-            var lastChar = inputVal[inputVal.length - 1];
-                console.log(lastChar);
-            if(operators.indexOf(lastChar) !== -1) {
+        else {
+            if (isInsertable(btnVal, inputVal)) {
                 input.innerHTML += btnVal;
             }
         }
-    }
-
+    };
 
     keys[i].onmouseover = function () {
         this.style.cursor = "hand";
-    }
+    };
+
     keys[i].onmouseout = function () {
         this.style.cursor = "pointer";
-    }
-
+    };
 }
 
+function isInsertable(btnVal, inputVal) {
+    var insertable;
+    console.log(btnVal, inputVal, Number(btnVal));
+    if (inputVal.length == 0) {
+        insertable = true;
+    }
+    else if (Number(btnVal) !== NaN) {
+        insertable = true;
+    }
+    else if(operators.indexOf(inputVal[inputVal.length - 1]) == -1) {
+        insertable = true;
+    }
+    else {
+        insertable = false
+    }
+    return insertable;
+}
